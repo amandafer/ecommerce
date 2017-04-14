bf1f2484b93ebc122a28facb9f4464656572d214
Amanda Fernandes <amandafer6@gmail.com>
2017-04-14 15:38:26 -0300

    Complete Firebase configuration

---
+++ Pods/Target Support Files/Pods-ecommerce/Pods-ecommerce-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ecommerce : NSObject
+@end
+@implementation PodsDummy_Pods_ecommerce
+@end
---
+++ ecommerce/Controller/SignInController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ecommerce
+//
+//  Created by Amanda Fernandes on 14/04/2017.
+//  Copyright © 2017 Amanda Fernandes. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SignInController: UIViewController {
+
+    @IBAction func signInFBBtn(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
